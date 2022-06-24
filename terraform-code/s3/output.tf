7c3797d4e078adbd4e39e5d3f44714c40d1f4d36
Dang Thuy Dung <dangthuydung.1909@gmail.com>
2022-06-24 09:26:19 +0700

    fix s3

---
+++ terraform-code/s3/output.tf
@@ -0,0 +1,4 @@
+output "bucket" {
+    value = aws_s3_bucket.laravel-bucket122abc 
+}
+
---
+++ terraform-code/s3/vars.tf
@@ -0,0 +1,8 @@
+variable "acl" {
+    default = "private"
+  
+}
+
+variable "bucket_name" {}
+
+variable "aws_vpc_endpoint" {}
